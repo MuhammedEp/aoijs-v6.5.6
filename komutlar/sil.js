5ba7db7911211272952d76c07d3b3c68410e5b19
Glitch (glitch-hello-node) <none>
2022-06-24 11:26:15 +0000

    👼🛋 Checkpoint ./komutlar/sil.js:43068238/266

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -2,7 +2,9 @@ module.exports = {
 name: "sil",
 aliases:["temizle"],
 code: `$clear[$message]
-$title[1;$message tane mesaj silindi]
+$onlyIf[$message!=;Lütfen silinecek mesaj sayısını giriniz]
+$description[1;$message tane mesaj silindi]
+$editIn[2s;$message tane mesajı uzaya attım by]
 $deleteIn[5s]
 $onlyPerms[admin;Yetkin yok kanki]`
 }
