b45aa1b5186e701fa24c0a815641f4f6f7bfc3f9
Glitch (glitch-hello-node) <none>
2022-06-24 20:32:58 +0000

    🎪🦀 Checkpoint ./server.js:43068238/1573 ./package.json:43068238/469 ./komutlar/test.js:43068238/254

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ intents: "all"
 bot.onMessage()
 
 
-
 //Bot açıldığında konsola gönderilecek komut
 bot.readyCommand({
     channel: "",
@@ -19,9 +18,3 @@ bot.readyCommand({
 
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd,"./komutlar/")
-
-bot.status.add({
-    name: "Client Status Messagw",
-    presence: "online",
-    type: "WATCHING",
-    duration: 2000
