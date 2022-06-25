df38df1f5c88080b745341d31a5260e42cfe5507
Glitch (glitch-hello-node) <none>
2022-06-25 21:07:54 +0000

    💕🎅 Checkpoint ./komutlar/çekilis.js:43068238/5785 ./komutlar/banlist.js:43068238/346

---
+++ komutlar/banlist.js
@@ -0,0 +1,12 @@
+module.exports = {
+name:"banlist",
+code:`
+$title[1;Yasaklı kişiler]
+$description[1;**Toplam \`$banCount\` Banlı Üye Var**
+
+$joinSplitText[
+]
+$textSplit[$usersBanned;, ]
+]
+`
+}
