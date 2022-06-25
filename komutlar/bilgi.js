81d3651e8383f40bc436a18283f0d22d25ea59ee
Glitch (glitch-hello-node) <none>
2022-06-25 21:43:54 +0000

    🎢👕 Checkpoint ./komutlar/bilgi.js:43068238/222 ./komutlar/reboot.js:43068238/2120 ./server.js:43068238/4

--- komutlar/bilgi.js
+++ komutlar/bilgi.js
@@ -22,12 +22,9 @@ $description[1;Selam ben **$userTag[$clientID]** işte istatisklerim ^^
 
 •🍓 Hesap kuruluş tarihim: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$clientID;time];years;Yıl;-1];months;Ay;-1];weeks; Hafta;-1];day;Gün;-1];hours;Saat;-1];minutes;Dakika;-1];and;Ve;-1];seconds;Saniye;-1] ** önce
 
-•🍉 Uptime sürem: **$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;saat;-1];minutes;dakika;-1];seconds;saniye;-1];hour;saat;-1];minute;dakika;-1];second;saniye;-1];and;ve;-1]**
+•🍉 Uptime sürem: $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$parseDate[$djseval[client.uptime;yes];time];hours;saat;-1];minutes;dakika;-1];seconds;saniye;-1];hour;saat;-1];minute;dakika;-1];second;saniye;-1];and;ve;-1]
 
-•🍒 Pingim: **$botPing**
-
-•🍏 Linkler: ** [Destek sunucum](https://discord.gg/FSFxzCXRFs) , [Davet linkim](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=8 )]
-
+•🍒 Pingim: $ping
 
 
 `
---
+++ komutlar/reboot.js
@@ -0,0 +1,7 @@
+module.exports = {
+name:"reboot",
+code:`
+$reboot[server.js] //main dosyanızın ismi gerekli server.js bot.js gibi
+$onlyForIDs[$botOwnerID;]
+`
+}
