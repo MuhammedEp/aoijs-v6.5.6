1e5c4840205adf03e834dabf5e1e701bfcbd80b6
Glitch (glitch-hello-node) <none>
2022-06-26 20:39:03 +0000

    🐯🎸 Checkpoint ./server.js:43068238/28 ./komutlar/sunucuinf.js:43068238/330

---
+++ komutlar/sunucuinf.js
@@ -0,0 +1,11 @@
+module.exports={
+name:"sunucu",
+aliases:["server"],
+code:`
+$author[1;$serverName[$guildID] sunucusu bilgileri;$serverIcon]
+$thumbnail[1;$serverIcon]
+$description[1;
+**Kanallar**[$channelCount[$guildID]]
+]
+`
+}
