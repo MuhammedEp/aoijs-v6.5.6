0ef5483a636871a9a7a0a3058cdce8b3992c370d
Glitch (glitch-hello-node) <none>
2022-06-26 11:03:58 +0000

    🌽🍡 Checkpoint ./komutlar/ban.js:43068238/192

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -2,7 +2,11 @@ module.exports={
 name:"ban",
 code:`
 $author[1;$userTag yasaklamak istiyormusun?;$authorAvatar;]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
 
-$description[1;sskk]
+$description[1;
+$addField[1;Yasaklanacak kişi:;$userTag[$mentioned[1]]]
+$addField[2;Yasaklanma sebebi:;$noMentionedMessage]
+]
 `
 }
