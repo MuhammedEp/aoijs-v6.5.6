869d45234837410109197962238520b848ccb696
Glitch (glitch-hello-node) <none>
2022-06-27 09:22:50 +0000

    🏥💍 Checkpoint ./komutlar/sunucuinf.js:43068238/106

--- komutlar/sunucuinf.js
+++ komutlar/sunucuinf.js
@@ -5,8 +5,14 @@ code:`
 $author[1;$serverName[$guildID] sunucusu bilgileri;$serverIcon]
 $thumbnail[1;$serverIcon]
 $description[1;
-**Kanallar**[$channelCount[$guildID]]
+**Kanallar**:[$channelCount[$guildID]]
 <:metinkanali:990718310205259888>$channelCount[$guildID;text] <:ses:990718268895559790>$channelCount[$guildID;voice] <:kategori:990718359270203392>$channelCount[$guildID;category]
+
+**Sunucu Sahibi**:
+<:tac:981253324584149002> $userTag[$ownerID]
+
+**Sunucu Bölgesi**:
+
 ]
 `
 }
