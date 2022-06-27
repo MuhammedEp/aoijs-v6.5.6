4713212c7085aa3a293d15ee50b0a8eda76cca67
Glitch (glitch-hello-node) <none>
2022-06-27 08:48:25 +0000

    🎌⚓️ Checkpoint ./komutlar/ban.js:43068238/765

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -7,14 +7,14 @@ $thumbnail[1;$userAvatar[$mentioned[1]]]
 
 $description[1;
 **Yasaklanacak kişi:**
-$userTag[$mentioned[1]]
+$message[1]
 
 **Yasaklanma sebebi:**
-$noMentionMessage
+$message[2]
 ]
-$onlyIf[$findUser[$mentioned[1];no]!=;<:reddet:973447718301216768> Lütfen yasaklanacak kişiyi etiketleyin.]
-$onlyIf[$mentioned[1]!=$serverOwner;<:reddet:973447718301216768> Sunucu sahibini yasaklamazsın.]
-$onlyIf[$mentioned[1]!=$authorID;Kendini yasaklamazsın knk.]
-$onlyIf[$noMentionMessage==;Lütfen yasaklama sebebi giriniz.]
+
+$onlyIf[$findUser[$message[1];no]!=;<:reddet:973447718301216768> Lütfen yasaklanacak kişiyi etiketleyin.]
+$onlyIf[$message[1]==$serverOwner;Sunucu sahibini yasaklayamazsın.]
+$argsCheck[>1;Lütfen yasakalanacak kişiyi etiketleyin.]
 `
 }
