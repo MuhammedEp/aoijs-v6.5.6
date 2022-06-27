fa01ecea222e2af88cc1d6635871050e0d8ab5a0
Glitch (glitch-hello-node) <none>
2022-06-27 14:24:14 +0000

    🚉⛈ Checkpoint ./komutlar/sunucuinf.js:43068238/194

--- komutlar/sunucuinf.js
+++ komutlar/sunucuinf.js
@@ -6,7 +6,7 @@ $author[1;$serverName[$guildID] sunucusu bilgileri;$serverIcon]
 $thumbnail[1;$serverIcon]
 $description[1;
 **Kanallar**:[$channelCount[$guildID]]
-<:metinkanali:990718310205259888>$channelCount[$guildID;text] <:ses:990718268895559790>$channelCount[$guildID;voice] <:kategori:990979997978214400>$channelCount[$guildID;category]
+<:metinkanali:990718310205259888>$channelCount[$guildID;Text] <:ses:990718268895559790>$channelCount[$guildID;Voice] <:kategori:990979997978214400>$channelCount[$guildID;Category]
 
 **Sunucu Sahibi**:
 <:tac:981253324584149002> $userTag[$ownerID]
@@ -15,12 +15,15 @@ $description[1;
 📆\`$creationDate[$guildID]\`
 
 **Sunucu Güvenlik Seviyesi**:
-$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$serverVerificationLevel;NONE;Yok;1];LOW;Düşük;1];MEDIUM;Orta;1];HIGH;Yüksek;1];VERY_;Çok;1]
+$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$serverVerificationLevel;NONE;Yok;1];LOW;Düşük;1];MEDIUM;Orta;1];HIGH;Yüksek;1];VERY_;Çok ;1]
 
 **Sunucu Bilgisi**:
 <:kullanici:977869477247082546> Toplam Üye: $membersCount
 <:boost:990915063089094696> Boost sayısı: $serverBoostCount
 <a:BAN:954984243816189962> Toplam Yasaklı: $banCount
+
+**Üyeler**:
+<:cevrimici:977696691601952768>$membersCount[$guildID;Online]<:bosta:977697844385443970>$membersCount[$guildID;İdle]<:mesgul:977697566332452874>$
 ]
 `
 }
