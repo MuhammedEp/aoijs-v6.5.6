652b58fb22498eb45f6a3f20ce85b839ea5f518d
Glitch (glitch-hello-node) <none>
2022-06-28 07:02:19 +0000

    🍄🚴 Checkpoint ./server.js:43068238/1219

--- server.js
+++ server.js
@@ -48,31 +48,3 @@ code: `
 $wait[1s]
 `
 })
-bot.command({
-  name: "menu",
-  code:`
-  Select an option.
-  
-  $addSelectMenu[1;helpCustomID;This placeholder won't show up cause we have selected default field as yes;1;1;no;A Option:Description of A option:helpValue0:no:👋;B Option::helpValue1:yes]
-  `
-});
-
-bot.interactionCommand({
-  name: "helpCustomID",
-  prototype: "selectMenu", 
-  code: `
-  $interactionUpdate[A option's response.;;{actionRow:{selectMenu:helpCustomID:Menu has been disabled:1:1:yes:{selectMenuOptions:This won't show up:helpValue0:Either this.:false}{selectMenuOptions:This won't show up either.:helpValue1:cause menu disabled.:false}}}]
-
-  $onlyIf[$interactionData[values[0]]==0;]
-  `
-});
-
-bot.interactionCommand({
-  name: "helpCustomID",
-  prototype: "selectMenu", 
-  code: `
-  $interactionUpdate[B option's response.;;{actionRow:{selectMenu:helpCustomID:Menu has been disabled:1:1:yes:{selectMenuOptions:This won't show up:helpValue0:Either this.:false}{selectMenuOptions:This won't show up either.:helpValue1:cause menu disabled.:false}}}]
-
-  $onlyIf[$interactionData[values[0]]==1;]
-  `
-});
