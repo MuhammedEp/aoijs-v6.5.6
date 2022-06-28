8228fe76227b986f5b8de660c46897432d030633
Glitch (glitch-hello-node) <none>
2022-06-28 06:50:25 +0000

    ⛱⛈ Checkpoint ./komutlar/hgbb-ayarla.js:43068238/124 ./server.js:43068238/3266

--- komutlar/hgbb-ayarla.js
+++ komutlar/hgbb-ayarla.js
@@ -4,13 +4,14 @@ aliases:["hgbb-ayarla"],
 code:`
 $setServerVar[hgbb;$mentionedChannels[1;yes]]
 $description[1;Giriş-çıkış kanalı <#$mentionedChannels[1;yes]> olarak ayarlandı.]
-
+$onlyPerms[admin;$username Yetkin Yok]
 `
 },{
 name:"giriş-çıkış-kapat",
 aliases:["hgbb-kapat"],
 code:`
 $setServerVar[hgbb;]
-$description[1;Giriş-çıkış kanalı s
+$description[1;Giriş-çıkış kanalı sıfırlandı.]
+$onlyPerms[admin;$username Yetkin Yok]
 `
 }]
