3954807bd68b487e0a3fe4da90ea51526b04e255
Glitch (glitch-hello-node) <none>
2022-07-01 06:19:18 +0000

    🐧🐷 Checkpoint ./komutlar/ban.js:43068238/4999 ./server.js:43068238/6

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,20 +1,26 @@
-module.exports={
-name:"ban",
+
+module.exports = {
+name: "ban",
 aliases:["yasakla"],
-code:`$reply
-$author[1;$userTag yasaklamak istiyormusun?;$authorAvatar;]
-$thumbnail[1;$userAvatar[$mentioned[1]]]
+code: `
+$ban[$guildID;$mentioned[1];0;$noMentionMessage[1]]
+$color[1;#2f3136]
+$title[1;İşlem Başarılı]
+$description[1;Bir sunucu üyesi sunucudan uzaklaştrıldı...]
+$addField[1;» Uzaklaştırma sebebi;› $if[$noMentionMessage==;\`Sebeb Belirtilmedi\`;\`$noMentionMessage\`]]
+$addField[1;» Uzaklaştıran yetkili;› <@$authorID>]
+$addField[1;» Uzaklaştırılan sunucu üyesi;› <@$mentioned[1]>]
+$image[1;https://c.tenor.com/20Bv1f8Vx30AAAAC/thor-banned-ban-hammer.gif]
+$thumbnail[1;$authorAvatar]
+$timezone[Turkey]
+$footer[1;$username Tarafından]
+$addTimeStamp[1]
 
-$description[1;
-**Yasaklanacak kişi:**
-$message[1]
-
-**Yasaklanma sebebi:**
-$message[2]
-]
-
-$onlyIf[$findUser[$message[1];no]!=;<:reddet:973447718301216768> Lütfen yasaklanacak kişiyi etiketleyin.]
-$onlyIf[$message[1]!=$ownerID;Sunucu sahibini yasaklayamazsın.]
-
+$onlyIf[$mentioned[1]!=$authorID;{newEmbed:{description:<@$authorID>, Kendini uzaklaştıramazsın.}{color:2f3136}}]
+$onlyIf[$isBanned[$guildID;$message[1]]==false;{newEmbed:{description:<@$authorID>, Kullanıcı zaten uzaklaştırılmış.}{color:2f3136}}]
+$onlyIf[$rolePosition[$highestRole[$clientID]]<=$rolePosition[$highestRole[$mentioned[1]]];{newEmbed:{description:<@$authorID>, Rolünden üstte/eşit birini uzaklaştıramazsın.}{color:2f3136}}]
+$onlyIf[$rolePosition[$highestRole[$authorID]]<=$rolePosition[$highestRole[$mentioned[1]]];{newEmbed:{description:<@$authorID>, Rolünden üstte/eşit birini uzaklaştıramazsın.}{color:2f3136}}]
+$onlyIf[$mentioned[1;no]!=undefined;{newEmbed:{description:<@$authorID>, Bir kullanıcı etiketlemelisin.}{color:2f3136}}]
+$onlyIf[$message!=;{newEmbed:{description:<@$authorID>, Kullanıcı belirt.}{color:2f3136}}]
 `
 }
