cd63a61ae0b70354f42787f80348bc18e01b9e79
Glitch (glitch-hello-node) <none>
2022-07-03 20:12:32 +0000

    🏥🛢 Checkpoint ./komutlar/uptime.js:43068238/1

--- komutlar/uptime.js
+++ komutlar/uptime.js
@@ -4,7 +4,7 @@ module . exports ={
 $color[1;RANDOM]
 $title[1;$username[$clientID] Botun Uptime Süresi]
 $description[1;
-Uptime sürem:  $replaceText[$replaceText[$replaceText[$replaceText[$uptime;hour;saat];minute;dakika];seconds;saniye];day;Gün]
+Uptime sürem:  $replaceText[$replaceText[$replaceText[$replaceText[$uptime;hour;saat];minutes;dakika];seconds;saniye];day;Gün]
 $thumbnail[1;$userAvatar[$clientID]]
 
 `
