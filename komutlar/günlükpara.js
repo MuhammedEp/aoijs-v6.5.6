89737b67d3a79cc4989517cb6678cdf3194051ae
Glitch (glitch-hello-node) <none>
2022-07-04 08:38:02 +0000

    💒🛢 Checkpoint ./komutlar/günlükpara.js:43068238/125

--- komutlar/günlükpara.js
+++ komutlar/günlükpara.js
@@ -2,17 +2,17 @@ module.exports={
 name:"günlük",
 code:`
 
-$color[1;RANDOM]
-$title[1;<a:coinnn:981248265603383416>Günlük Para<a:coinnn:981248265603383416>]
+$color[1;#ffff]
+$title[1;Günlük Para]
 
 $description[1;
-***Hesabına yatırlan toplam para:*** $numberSeparator[$sum[$getGlobalUserVar[mpara;$authorID];$random[2500;10000]]]
+***Hesabına yatırlan toplam para:*** $sum[$getGlobalUserVar[mpara;$authorID];$random[2500;10000]]
 $setGlobalUserVar[para;$sum[$getGlobUserVar[mpara;$authorID];$random[2500;10000];$getGlobalUserVar[para;$authorID]];$authorID]
 
 ***Mevcut Mesleğin:***$getGlobalUserVar[meslek;$authorID]
 **Maaş:**$numberSeparator[$getGlobalUserVar[mpara;$authorID]]]
 
-$cooldown[1; <@$authorID>,\`bu komutu kullanmak için %time% beklemelisin.\`]
+$cooldown[1m; $username,\`bu komutu kullanmak için %time% beklemelisin.\`]
 
 `
 }
