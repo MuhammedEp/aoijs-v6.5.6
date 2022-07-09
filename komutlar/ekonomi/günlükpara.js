80faf81f633672eccc2a9ff126e698aeb57d1d7d
Glitch (glitch-hello-node) <none>
2022-07-09 13:28:48 +0000

    🌁📒 Checkpoint ./komutlar/ekonomi/günlükpara.js:43068238/407

--- komutlar/ekonomi/günlükpara.js
+++ komutlar/ekonomi/günlükpara.js
@@ -3,9 +3,12 @@ name:"günlük",
 code:`
 $title[1;Günlük paran]
 $description[1;
-***Hesabına yatırılan toplam para:***$numberSeparator[$sum[$random[15000;25000];$getGlobalUserVar[mpara;$authorID]]]
-$set
+***Hesabına yatırılan toplam para:*** $numberSeparator[$sum[$random[15000;25000];$getGlobalUserVar[mpara;$authorID]]]
+$setGlobalUserVar[para;$sum[$random[15000;25000];$getGlobalUserVar[para;$authorID];$getGlobalUserVar[mpara;$authorID]];$authorID]
 
+***Mevcut Mesleğin:***$getGlobalUserVar[meslek;$authorID]
+**Maaş:**$getGlobalUserVar[mpara;$authorID]]
+$replaceText[$rel$cooldown[24h;%time% süre beklemelisin]
 
 `
 }
