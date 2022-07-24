0f4246756f5bb24a1ba58e77ea39a3f8d1256b03
Glitch (glitch-hello-node) <none>
2022-07-24 10:08:14 +0000

    🎒👣 Checkpoint ./komutlar/genel/botinf.js:43068238/217

--- komutlar/genel/botinf.js
+++ komutlar/genel/botinf.js
@@ -2,12 +2,20 @@ module.exports={
 name:"istatistik",
 aliases:["i","bot","botbilgi"],
 code:`
-$addField[1;Gecikmeler;
+$addField[1;Gecikmeler:;
 **Ping:** $pingms
 **Mesaj Ping:** $executionTimems]
 
-$addField[1;Node Sürümü;$nodeVersion]
+$addField[1;Node Sürümü:;$nodeVersion]
 
-$addField[1;Ram;$ram[heapTotal]]
+$addField[1;Ram:;$ram[heapTotal]]
+
+$addField[1;Kullanıcı Sayısı:;$allMembersCount]
+
+$addField[1;Sunucu Sayısı:;$serverCount]
+
+$addField[1;Kütüphane:;Glitch[Aoi.js]]
+
+$addField[1;$Bot Sahibi
 `
 }
