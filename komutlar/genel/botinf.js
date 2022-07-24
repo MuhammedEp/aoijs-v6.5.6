198334f94bc7b2098dcb1705b6cc24e39753f17b
Glitch (glitch-hello-node) <none>
2022-07-24 10:32:18 +0000

    ✌️💏 Checkpoint ./komutlar/genel/botinf.js:43068238/66

--- komutlar/genel/botinf.js
+++ komutlar/genel/botinf.js
@@ -18,5 +18,9 @@ $addField[1;Sunucu Sayısı:;$serverCount]
 $addField[1;Kütüphane:;Glitch[Aoi.js]]
 
 $addField[1;Bot Sahibi:;[$userTag[$botOwnerID]](https://discord.com/users/$botOwnerID)]
+
+$addField[1;Yapım tarihi:;$creationDate[$clientID]]
+
+$author
 `
 }
