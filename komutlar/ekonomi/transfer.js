a513b14341d63a855b626c99426fe6c052f5aa38
Glitch (glitch-hello-node) <none>
2022-07-29 18:57:40 +0000

    🚆👆 Checkpoint ./komutlar/ekonomi/transfer.js:43068238/389

--- komutlar/ekonomi/transfer.js
+++ komutlar/ekonomi/transfer.js
@@ -9,7 +9,11 @@ $setGlobalUserVar[para;$sub[$getGlobalUserVar[para;$authorID];$noMentionMessage]
 $color[1;$random[000000;999999]]
 $title[1;Para Transferi Başarılı]
 $description[1;💳 | <@$mentioned[1]> adlı kullanıcıya $numberSeparator[$noMentionMessage] <:MacCoin:1000805779592458370> MacCoin gönderdin.]
-$onlyIf[$mentioned[1]!=$authorID;Bu sensin ]
+$onlyIf[$mentioned[1]!=$authorID;{newEmbed:{description:Kendine para göndermezsin.}{color:RED}} ]
+$onlyIf[$noMentionMessage!=;{newEmbed:{description:Bir miktar belirtmelisin.}{color:GREEN}}]
+$onlyIf[$userExists[$mentioned[1]]==false;Bir kullanici belirt]
+$onlyIf[$mentioned[1]!=;{newEmbed:{description:Bir kullanıcı etiketlemelisin.}
+
 
 `
 }
