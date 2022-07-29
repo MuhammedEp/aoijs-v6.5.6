2557c7085f2f442e25204407ec59a6d10936e001
Glitch (glitch-hello-node) <none>
2022-07-29 20:43:28 +0000

    🕷🎽 Checkpoint ./komutlar/panel/panel-kur.js:43068238/71 ./komutlar/panel/panelkur.js:43068238/133

--- komutlar/panel/panel-kur.js
+++
@@ -1,9 +0,0 @@
-module.exporst=({
-name:"panel-kur",
-code:`
-$onlyPerms[admin;]
-$description[1;Üye panel kuruldu]
-$createChannel
-
-`
-})
---
+++ komutlar/panel/panelkur.js
@@ -0,0 +1,8 @@
+module.exports={
+name:"panel",
+code:`
+$description[1;Üye panel kuruldu]
+$createChannel[$guildID;Üye Sayısı•$membersCount;Voice;yes]
+
+`
+}
