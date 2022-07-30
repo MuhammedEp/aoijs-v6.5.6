fba6980b96be0f6c6ccf9764185ddffd6b7a048a
Glitch (glitch-hello-node) <none>
2022-07-30 10:08:43 +0000

    👐😅 Checkpoint ./komutlar/genel/yardım.js:43068238/298

--- komutlar/genel/yardım.js
+++ komutlar/genel/yardım.js
@@ -2,7 +2,10 @@ module.exports={
 name:"yardım",
 aliases:["help",""],
 code:`
-sa
+$thumbnail[1;$authorAvatar]
+$author[1;$userTag[$clientID] komutları;$userAvatar[$clientID]]
+$footer[1;Sorgulayan $userTag;$authorAvatar]
+$description[1; Deneme]
 
 `
 }
