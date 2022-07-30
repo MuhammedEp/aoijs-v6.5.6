60de0ae938a974bd56b0b40c8d85a487772977fa
Glitch (glitch-hello-node) <none>
2022-07-30 06:13:29 +0000

    ⛳️🐙 Checkpoint ./komutlar/mod/banlist.js:43068238/724

---
+++ komutlar/mod/banlist.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "banlist",
+  code: `
+$title[1;$serverName Sunucusundan Yasaklananlar]
+$description[1;
+Bu Sunucuda Toplam \`$banCount\` Tane Yasaklı Kullanıcı Var **#SEMI#**
+$djsEval[message.guild.bans.fetch().then(banlılar => banlılar.map(üye => "\`------------------------\`" + "\\n" + "**Yasaklanan Kişi**:" + " " + "<" + "@" + üye.user.id + ">" + " " + "~" + " " + üye.user.tag + " " + "~" + " " + üye.user.id + " " + "|" + " " + "**Yasaklanma Nedeni**:" + " " + üye.reason).join("\\n"));yes]
+]
+$color[1;RED]
+$thumbnail[1;$serverIcon[$guildID]]
+$footer[1;$userTag[$authorID] tarafından istendi]
+$addTimestamp[1]
+$onlyPerms[admin;Bu komutu kullanabilmek için \`Yönetici\` İznine Sahip Olman Gerekiyor!]
+`
+}  
