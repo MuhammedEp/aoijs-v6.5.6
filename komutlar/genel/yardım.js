2045dcb1b37cce2349d2c4de1f4fe0f2dfed1311
Glitch (glitch-hello-node) <none>
2022-07-30 10:19:47 +0000

    🍽👙 Checkpoint ./komutlar/genel/yardım.js:43068238/209

--- komutlar/genel/yardım.js
+++ komutlar/genel/yardım.js
@@ -5,7 +5,17 @@ code:`
 $thumbnail[1;$authorAvatar]
 $author[1;$userTag[$clientID] komutları;$userAvatar[$clientID]]
 $footer[1;Sorgulayan $userTag;$authorAvatar]
-$description[1; Deneme]
+$description[1;
+$addField[1;Moderasyon Komutları;\`\`\`
+📗$getServerVar[prefix]ban
+📗$getServerVar[prefix]unban
+📗$getServerVar[prefix]sil]
 
+
+
+
+
+
+]
 `
 }
