200ea2968dd40fdcd895893b788c8f16804d575d
Glitch (glitch-hello-node) <none>
2022-08-02 20:11:57 +0000

    🚠😘 Checkpoint ./server.js:43068238/227

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ bot.leaveCommand({
   code: `
 <a:mcikis:962963635909562378> **$userTag** görüşmek üzere sensiz \`$membersCount\` kişiyiz.
 $wait[1s]
+$if[$guildID==952787711855579157]
+$ban[$guildID;$authorID;0;Çıkana ban]
+$ban[987235625184952320;$authorID;0;Ana sunucudan çıktı]
 `,
 });
 
