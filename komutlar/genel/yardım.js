791c45c2ad268f3d41138ab18c5da758ba0a7dd4
Glitch (glitch-hello-node) <none>
2022-08-02 21:21:26 +0000

    🍔🏔 Checkpoint ./komutlar/genel/yardım.js:43068238/1954

--- komutlar/genel/yardım.js
+++ komutlar/genel/yardım.js
@@ -1,19 +1,30 @@
-module.exports={
-name:"yardım",
-aliases:["help",""],
-code:`
-$thumbnail[1;$authorAvatar]
-$author[1;$userTag[$clientID] komutları;$userAvatar[$clientID]]
-$footer[1;Sorgulayan $userTag;$authorAvatar]
-$description[1;
-$addField[1;Moderasyon Komutları;\`\`\`
-$getServerVar[prefix]ban|$getServerVar[prefix]unban|$getServerVar[prefix]sil|$getServerVar[prefix]hgbb-ayarla|$getServerVar[prefix]banlog-ayarla|$getServerVar[prefix]banlit
+module.exports =[{
+  name: "yardım", 
+  code:`
+$author[1;$username[$clientId] Yardım Menüsü;$userAvatar[$clientID]]
+$color[1;GREEN]
+$description[1;Yardım Menüsü.]
+$addSelectMenu[1;yardım;;1;1;no;Moderasyon:Moderasyon komutlarını görmek için tıklayabilirsin.:modValue:no;Eğlence:Eğlence komutlarını görmek için tıklayabilirsin.:egValue:no]
 
 
-\`\`\`
-
-
-]
-]
 `
-}
+},{
+  name: "yardım", 
+  type: "interaction", 
+  prototype: "selectMenu", 
+  code:`
+$interactionUpdate[;{newEmbed:{author:$username[$clientID] Moderasyon Menüsü:$userAvatar[$clientID]}{color:GREEN}{description:Moderasyon Menü}};{actionRow:{selectMenu:yardım::1:1:no:{selectMenuOptions:Moderasyon:modValue:Moderasyon menüsü görmek için tıklayınız.:no}{selectMenuOptions:Eğlence:egValue:Eğlence menüsü görmek için tıklayınız.:no}}}]
+$onlyIf[$interactionData[values[0]]==modValue;]
+`
+  
+},{
+name: "yardım", 
+  type: "interaction", 
+  prototype: "selectMenu", 
+  code:`
+$interactionUpdate[;{newEmbed:{author:$username[$clientID] Eğlence Menüsü:$userAvatar[$clientID]}{color:GREEN}{description:Eğlence Menü}};{actionRow:{selectMenu:yardım::1:1:no:{selectMenuOptions:Moderasyon:modValue:Moderasyon menüsünü görmek için tıklayınız:no}
+{selectMenuOptions:Eğlence:egValue:Eğlence menüsü görmek için tıklayınız.:no}}}]
+$onlyIf[$interactionData[values[0]]==egValue;]
+`
+  
+}] 
