f204f2b3d540f52ba7b54c68c3c3d56168bb44f2
Glitch (glitch-hello-node) <none>
2022-08-12 07:39:59 +0000

    👁🛫 Checkpoint ./komutlar/kullanıcı/userinf.js:43068238/333

---
+++ komutlar/kullanıcı/userinf.js
@@ -0,0 +1,10 @@
+module.exports={
+name:"kullanıcı",
+aliases:["kb","user"],
+code:`
+$thumbnail[1;$mentioned[1;yes]]
+$author[1;$userTag[$mentioned[1;yes]] adlı kişinin bilgileri;$userAvatar[$mentioned[1;yes]]]
+$footer[1;Sorgulayan $userTag;$authorAvatar]
+$description[1;etst]
+`
+}
