364b1c4adf20255eb3b1f091cabe2ee88f084780
Glitch (glitch-hello-node) <none>
2022-08-12 08:14:16 +0000

    ☎️👬 Checkpoint ./komutlar/kullanıcı/userinf.js:43068238/212

--- komutlar/kullanıcı/userinf.js
+++ komutlar/kullanıcı/userinf.js
@@ -6,8 +6,8 @@ $thumbnail[1;$userAvatar[$mentioned[1;yes]]]
 $author[1;$userTag[$mentioned[1;yes]] adlı kişinin bilgileri;$userAvatar[$mentioned[1;yes]]]
 $footer[1;Sorgulayan $userTag ;$authorAvatar]
 $description[1;
-$addField[Rozetler;$getUserBadges]
-$if[$
+$addField[Rozetler;$replaceText[$replaceText[$replaceText[$replaceText[$getUserBadges[$mentioned[1;yes];HOUSE_BALANCE;]
+$if[$hasPerms[$guildID;$mentioned[1;yes];admin]==true;$addField[Üye izinleri;Yönetici];]
 
 
 ]
