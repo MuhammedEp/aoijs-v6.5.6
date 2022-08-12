ab3cf95c2b0937520dde4dba4541538b0a3696af
Glitch (glitch-hello-node) <none>
2022-08-12 08:03:01 +0000

    🍣📍 Checkpoint ./komutlar/kullanıcı/userinf.js:43068238/104

--- komutlar/kullanıcı/userinf.js
+++ komutlar/kullanıcı/userinf.js
@@ -2,9 +2,14 @@ module.exports={
 name:"kullanıcı",
 aliases:["kb","user"],
 code:`
-$thumbnail[1;$mentioned[1;yes]]
+$thumbnail[1;$userAvatar[$mentioned[1;yes]]]
 $author[1;$userTag[$mentioned[1;yes]] adlı kişinin bilgileri;$userAvatar[$mentioned[1;yes]]]
-$footer[1;Sorgulayan $userTag;$authorAvatar]
-$description[1;etst]
+$footer[1;Sorgulayan $userTag ;$authorAvatar]
+$description[1;
+$addField[Rozetler;$getUserBadges]
+$if[$
+
+
+]
 `
 }
