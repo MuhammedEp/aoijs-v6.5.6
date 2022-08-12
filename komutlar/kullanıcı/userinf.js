882152ee444cb14de6a57e1fc65d715baf4a98c4
Glitch (glitch-hello-node) <none>
2022-08-12 09:02:19 +0000

    🐑🏢 Checkpoint ./komutlar/kullanıcı/userinf.js:43068238/46

--- komutlar/kullanıcı/userinf.js
+++ komutlar/kullanıcı/userinf.js
@@ -9,7 +9,7 @@ $color[1;GREEN]
 $description[1;
 $addField[Rozetler;> $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$getUserBadges[$mentioned[1;yes]];HOUSE_BALANCE;<a:balance:1007562254465650789>;1];HOUSE_BRILLIANCE;<a:brilliance:1007562598994149467>;1];HOUSE_BRAVERY;<a:bravery:1007562832268754946>;1]
 ;VERIFIED_BOT;<a:onayl_bot:1007563011185188944>;1];none;Yok;1]]
-$addField[Üye izinleri;$if[$hasPerms[$guildID;$mentioned[1;yes];admin]==true;Yönetici;Üye]
+$addField[Üye izinleri;$if[$hasPerms[$guildID;$mentioned[1;yes];admin]==true;Yönetici;$if[$hasPerms[$guildID;$ment]
 
 
 ]
