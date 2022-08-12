ac2957ec0ca645e8f5de5a9cb56090efccab36db
Glitch (glitch-hello-node) <none>
2022-08-12 17:01:46 +0000

    🕌👆 Checkpoint ./komutlar/ranksistemi/Xp.js:43068238/118

--- komutlar/ranksistemi/Xp.js
+++ komutlar/ranksistemi/Xp.js
@@ -1,9 +1,11 @@
 module.exports={
 name:"$alwaysExecute",
-code:`$onlyForIDs[$botOwnerID;]
+code:`
 $setUserVar[xp;$sum[$getUserVar[xp;$authorID];$getServerVar[xpm]];$authorID]
 $if[$getUserVar[xp;$authorID]>=5000;$setUserVar[seviye;$sum[$getUserVar[seviye;$authorID];1];$authorID]
 $setUserVar[xp;$sub[$getUserVar[xp;$authorID];5000];$authorID]
-$channelSendMessage[$getServerVar[seviyeknl];<@$authorID> seviye atladın $getUserVar[seviye;$authorID] seviyeye ulaştın.]
+$useChannel[$getServerVar[seviyeknl]]
+<@$authorID> seviye atladın $getUserVar[seviye;$authorID] seviyeye ulaştın.]
+$suppressErrors
 `
 }
