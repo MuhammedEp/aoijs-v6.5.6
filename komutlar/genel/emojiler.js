32a1062f1c167cce67f5af0c0f8571a3183c9c97
Glitch (glitch-hello-node) <none>
2022-08-13 08:41:47 +0000

    👞⛪️ Checkpoint ./komutlar/genel/emojiler.js:43068238/180

--- komutlar/genel/emojiler.js
+++ komutlar/genel/emojiler.js
@@ -4,7 +4,7 @@ module.exports = [{
   code:`
   $setMessageVar[id;$authorID;$get[id]]
   $let[id;$apiMessage[$channelID;;{newEmbed:{color:GREEN}{author:$serverName[$guildID] Sunucusunun Emojileri:$serverIcon}{thumbnail:$authorAvatar}{footer:Sorgulayan $userTag:$authorAvatar}{description:
-  $serverEmojis
+  $serverEmojis[]
   }};{actionRow:{button:Emojileri Winrarla:1:winrar:no:📚}{button:Sil:2:sil:no:🗑️};;;;;yes]]
   `
 },{
@@ -12,8 +12,9 @@ module.exports = [{
   $if: "v4",
   type:"interaction",
   prototype:"button",
-  code:`$interactionDefer[yes]
-  $createFile[$serverEmojis;emojiler.zip]
+  code:`$interactionReply[$onlyPerms[admin;Sadece Yöneticiler]
+  $createFile[$serverEmojis[];emojiler.js]]
+  $interactionDeferUpdate
 
 
   
