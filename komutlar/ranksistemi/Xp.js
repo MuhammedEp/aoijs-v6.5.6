d39dc2dafc93749ab9348a91e3d1708de9337019
Glitch (glitch-hello-node) <none>
2022-08-13 10:15:50 +0000

    🎤🌋 Checkpoint ./komutlar/ranksistemi/Xp.js:43068238/76

--- komutlar/ranksistemi/Xp.js
+++ komutlar/ranksistemi/Xp.js
@@ -6,7 +6,7 @@ $if[$getUserVar[xp;$authorID]==5000;
 $setUserVar[xp;$sub[$getUserVar[xp;$auyhorID];5000];$authorID]
 $setUserVar[seviye;$sum[$getUserVar[seviye;$authorID];1];$authorID]
 $useChannel[$getServerVar[seviyeknl]]
-<@$authorID> seviye atladın $getUserVar[seviye;$authorID] seviyeye ulaştın.]
+<@$authorID> seviye atladın $getUserVar[seviye;$authorID] seviyeye ulaştın.;$setUserVar[xp;$sum[$getUserVar[xp;$authorID];$getServerVar[xpm]];$au]
 $suppressErrors
 `
 }
