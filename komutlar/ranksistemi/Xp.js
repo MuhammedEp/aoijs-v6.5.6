e71b2bdc09a8566b00cd49c80a2714653dd2cea7
Glitch (glitch-hello-node) <none>
2022-08-13 09:51:49 +0000

    📇🚶 Checkpoint ./komutlar/ranksistemi/Xp.js:43068238/153

--- komutlar/ranksistemi/Xp.js
+++ komutlar/ranksistemi/Xp.js
@@ -2,8 +2,9 @@ module.exports={
 name:"$alwaysExecute",
 code:`
 $setUserVar[xp;$sum[$getUserVar[xp;$authorID];$getServerVar[xpm]];$authorID]
-$if[$getUserVar[xp;$authorID]>=5000;$setUserVar[seviye;$sum[$getUserVar[seviye;$authorID];1];$authorID]
-$setUserVar[xp;$sub[$getUserVar[xp;$authorID];5000];$authorID]
+$if[$getUserVar[xp;$authorID]==5000;
+$setUserVar[xp;$sub[$getUserVar[xp;$auyhorID];5000];$authorID]
+$setUserVar[seviye;$sum[$getUserVar[seviye;$authorID];1];$authorID]
 $useChannel[$getServerVar[seviyeknl]]
 <@$authorID> seviye atladın $getUserVar[seviye;$authorID] seviyeye ulaştın.]
 $suppressErrors
