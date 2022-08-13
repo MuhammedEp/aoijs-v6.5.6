6b3dd21d05c84f624c40617429bc6cd51a06eb60
Glitch (glitch-hello-node) <none>
2022-08-13 07:53:46 +0000

    🐗🔖 Checkpoint ./komutlar/genel/emojiler.js:43068238/443

--- komutlar/genel/emojiler.js
+++ komutlar/genel/emojiler.js
@@ -4,10 +4,18 @@ code:`
 $author[1;$serverName[$guildID] Sunucusunun Emojileri;$serverIcon]
 $footer[1;Sorgulayan: $userTag;$authorAvatar]
 $description[1;$serverEmojis]
-$createFile[$joinSplitText[
-]
-$textSplit[$serverEmoji; ];]
+
 $addButton[1;Emojileri winrarla;secondary;winrar;no;📚]
 `
 },{
-name:"
+name:"winrar",
+type:"interaction",
+prototype:"button",
+code:`
+$createFile[$joinSplitText[
+]
+$textSplit[$serverEmojis;];emojiler.rar]
+$onlyPerms[admin;Sadece Yöneticiler]
+$ephemeral
+`
+}]
