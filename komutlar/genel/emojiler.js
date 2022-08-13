557ad09cdd9af8551cd66937b78460c0720c1335
Glitch (glitch-hello-node) <none>
2022-08-13 07:41:48 +0000

    ⛸👌 Checkpoint ./komutlar/genel/emojiler.js:43068238/299

--- komutlar/genel/emojiler.js
+++ komutlar/genel/emojiler.js
@@ -1,8 +1,13 @@
-module.exports={
+module.exports=[{
 name:"emojiler",
 code:`
 $author[1;$serverName[$guildID] Sunucusunun Emojileri;$serverIcon]
 $footer[1;Sorgulayan: $userTag;$authorAvatar]
-$description[1;$replaceserverEmojis]
+$description[1;$serverEmojis]
+$createFile[$joinSplitText[
+]
+$textSplit[$serverEmoji; ];]
+$addButton[1;Emojileri winrarla;secondary;winrar;no;📚]
 `
-}
+},{
+name:"
